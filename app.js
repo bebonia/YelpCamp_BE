d78295c564804ec0817ef9fb6415bb44d97dab27
bebonia <bebonia@gmail.com>
2018-01-28 22:31:51 -0500

    add mLab server

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+//mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb://bien:neib1234@ds117878.mlab.com:17878/yelpcampbe");
+//mongodb://bien:neib1234@ds117878.mlab.com:17878/yelpcampbe
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -48,7 +50,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-
+//process.env.PORT, process.env.IP
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
